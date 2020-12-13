afc6f580420a674f176b8d07b6d82ea91af83e4b
Kaijie Zhang <kaijiezhang22@gmail.com>
2020-12-14 00:01:48 +0100

    views, partials, and 404

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,66 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.set('view engine', '')
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Kaijie Zhang'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Kaijie Zhang'
+    })
+})
 
 app.get('/weather',  (req, res) => {
     res.send({
         forecast: '37deg',
         location: 'Frankfurt'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'This is the help page',
+        title: 'Help',
+        name: 'Kaijie Zhang'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        message: 'Help article not found.',
+        title: '404',
+        name: 'Kaijie Zhang'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        message: 'Page not found.',
+        title: '404',
+        name: 'Kaijie Zhang'
     })
 })
 
