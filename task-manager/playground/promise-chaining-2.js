0608e760d061aa1459be933534e9c5b683a7bbbc
Kaijie Zhang <kaijiezhang22@gmail.com>
2021-01-10 17:50:25 +0100

    use routers

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -4,8 +4,20 @@ const Task = require ('../src/models/task')
 Task.findByIdAndDelete('5febbd7a09dabb0c9e11653a').then((task) => {
     console.log(task)
     return Task.countDocuments({completed: false})
-}).then((result) => {
+}).then((result) => {  
     console.log(result)
 }).catch((e) => {
     console.log(e)
+})
+
+const deleteTaskAndCount = async(id) => {
+    await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed: false})
+    return count
+}
+
+deleteTaskAndCount('5febbd8409dabb0c9e11653b').then((count) =>{
+    console.log('count:', count)
+}).catch((e)=>{
+    console.log('e', e)
 })
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -3,11 +3,23 @@ const User = require ('../src/models/user')
 
 //5febbd7a09dabb0c9e11653a
 
-User.findByIdAndUpdate('5febbb96c007dd0bcd640560', {age : 1}).then((user) => {
-    console.log(user)
-    return User.countDocuments({age: 1})
-}).then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate('5febbb96c007dd0bcd640560', {age : 1}).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({age: 1})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({age})
+    return count
+}
+
+updateAgeAndCount('5febbb96c007dd0bcd640560', 2).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
